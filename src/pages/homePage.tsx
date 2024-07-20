e360c1829d0f9f9107da112775aac1622cdc2705
neal-boylan <nealboylan@gmail.com>
2024-07-20 19:21:40 +0100

    Add to favourites logic completed; Dummy favourites page

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -39,6 +39,13 @@ const MovieListPage: React.FC= () => {
     else setGenreFilter(value);
   };
 
+  const addToFavourites = (movieId: number) => {
+    const updatedMovies = movies.map((m: BaseMovieProps) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -61,7 +68,7 @@ const MovieListPage: React.FC= () => {
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+          <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
         </Grid>
       </Grid>
       <Fab
