f210cc329ac3916b63bcc36659382d050cc94ed6
neal-boylan <nealboylan@gmail.com>
2024-07-20 13:28:27 +0100

    Added routing and live data from API

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -15,6 +15,7 @@ export interface BaseMovieProps {
 	revenue: number;
 	vote_count: number;
 	favourite?: boolean;
+	genre_ids?: number[];
 }
 
 export interface BaseMovieListProps { 
@@ -48,3 +49,5 @@ export interface MoviePageProps {
   movie: MovieDetailsProps;
   images: MovieImage[];
 }
+
+export type FilterOption = "title" | "genre";
