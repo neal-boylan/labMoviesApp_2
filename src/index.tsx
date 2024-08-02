9ec4441fc8c5417f29adbc09e0eade6873fae7e9
neal-boylan <nealboylan@gmail.com>
2024-08-02 23:47:47 +0100

    Full review page

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <ul>
+      <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -19,6 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
@@ -26,8 +28,8 @@ const App = () => {
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
